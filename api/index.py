00011de64938b4be8e066243ea12f4af1c5b0e68
Trebhuvan SB <trebhuvan.sb@circularedge.com>
2025-01-20 15:27:20 +0530

    Initialization

---
+++ api/index.py
@@ -0,0 +1,10 @@
+import json
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+        return
